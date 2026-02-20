7fcc9de265f94c442124e8e659246f6303dd57f4
CristelBM <millan.cristel@gmail.com>
2026-02-20 10:34:35 +0800

    Initial Cypress automation setup

---
+++ cypress.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  allowCypressEnv: false,
+
+  e2e: {
+    baseUrl: process.env.CYPRESS_BASE_URL || "http://localhost:3000",
+    specPattern: "cypress/e2e/**/*.cy.js",
+
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions: {
+      reportDir: "reports/mocha",
+      overwrite: false,
+      html: false,
+      json: true,
+    },
+
+    setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
+      return config;
+    },
+  },
+});
